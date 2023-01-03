bdc1a25b0588a61d4acac9d92e96638a48d8f23d
Mominadil <mominadil864@gmail.com>
2023-01-03 14:11:50 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import IndexScreen from './src/screens/IndexScreen';
+import { BlogProvider } from './src/context/BlogContext';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'rgb(255, 45, 85)',
+    background: 'white'
   },
-});
+};
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <BlogProvider>
+      <NavigationContainer theme={MyTheme}>
+        <Stack.Navigator>
+          <Stack.Screen name="IndexScreen" component={IndexScreen} options={{ title: 'Blogs', cardStyle: { backgroundColor: 'transparent' } }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </BlogProvider>
+    );
+  }
+
+  export default App;
---
+++ src/context/BlogContext.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({ children }) => {
+
+    const [blogPosts, setBlogPosts] = useState([])
+
+    const addBlogPost = () => {
+        setBlogPosts([
+        	...blogPosts, 
+        	{ title: `Blog Post #${blogPosts.lenght + 1}` }
+        ]);
+    };
+
+    return (
+    	<BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
+    		{children}
+    	</BlogContext.Provider>
+    );
+};
+
+
+export default BlogContext;
