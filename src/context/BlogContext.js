939843c83ad8656acf11b96a473b49ac8a66440a
Mominadil <mominadil864@gmail.com>
2023-01-04 16:08:24 +0530

    Use resucer using intead of use state

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,18 +1,25 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text } from 'react-native';
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+    switch(action.type) {
+        case 'add_blogpost' :
+            return [...state, {title: `Blog Post #${state.length + 1}`}];
+        default:
+            return state;
+    }   
+};
+
 export const BlogProvider = ({ children }) => {
 
-    const [blogPosts, setBlogPosts] = useState([])
+    const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
-    const addBlogPost = () => {
-        setBlogPosts([
-        	...blogPosts, 
-        	{ title: `Blog Post #${blogPosts.length + 1}` }
-        ]);
+    const addBlogPost = () =>{
+        dispatch({type:'add_blogpost'});
     };
+    
 
     return (
     	<BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
