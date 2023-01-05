986df21e10e60f1c26b794b25170a0a8cc91da72
mominadil <mominadil864@gmail.com>
2023-01-05 18:52:25 +0530

    creating create screen

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { Context } from '../context/BlogContext';
+
+
+const CreateScreen = ({navigation}) => {
+	return (
+		<View>
+			<Text>create screen</Text>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	
+})
+
+export default CreateScreen
